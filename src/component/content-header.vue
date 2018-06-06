ce9902372efdf0d067ef935144f6e37493c6a448
chenzhengen <zhg.chen@muheda.com>
2018-06-06 18:45:47 +0800

    提交修改

--- src/component/content-header.vue
+++ src/component/content-header.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>{{title}}</h1>
+        <h1>{{title}}</h1>
 </template>
 <script>
 export default {
@@ -18,3 +18,7 @@ export default {
     }
 }
 </script>
+<style scoped>
+    
+</style>
+
